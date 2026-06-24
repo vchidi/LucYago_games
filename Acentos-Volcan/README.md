214c57665f8b4973080ba58d770d9fa12520a91c
vchidi <97399702+vchidi@users.noreply.github.com>
2026-06-24 22:36:20 +0200

    Add files via upload

---
+++ Acentos-Volcan/README.md
@@ -0,0 +1,27 @@
+# Acentos-Volcan
+
+Juego HTML para practicar acentuación en español.
+
+## Subir a GitHub
+
+Copiar esta carpeta dentro de:
+
+LucYago_Games/Acentos-Volcan
+
+## Archivo principal
+
+index.html
+
+## Integración con menú principal
+
+Desde el menú principal enlazar a:
+
+Acentos-Volcan/index.html
+
+## Características
+
+- Optimizado para iPad
+- Volcán dinámico
+- Banco editable de palabras
+- Modo revisión de errores
+- Preparado para otros idiomas
---
+++ Multiplicaciones-Pecera/sw.js
@@ -0,0 +1,27 @@
+const CACHE = "multiplicaciones-iphone-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./apple-touch-icon.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(found => found || fetch(event.request))
+  );
+});
