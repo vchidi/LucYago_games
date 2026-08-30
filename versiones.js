7ed7edf9e4f7b6f650226ebded5f1683f36d8662
vchidi <97399702+vchidi@users.noreply.github.com>
2026-08-30 21:27:50 +0200

    Update LucYago games

---
+++ versiones.js
@@ -0,0 +1,11 @@
+window.LucYagoVersions=[
+  {name:"Menu principal",path:"index.html",date:"2026-08-15"},
+  {name:"Reto mixto",path:"reto_mixto.html",date:"2026-08-20"},
+  {name:"Irregular Verbs",path:"IrregularVerbs-Ahorcado/index.html",date:"2026-08-15"},
+  {name:"Multiplicaciones",path:"Multiplicaciones-Pecera/index.html",date:"2026-08-20"},
+  {name:"Football Empire",path:"Football_Empire-Maths/index.html",date:"2026-08-30"},
+  {name:"Spelling Football",path:"Spelling-Football/index.html",date:"2026-08-30"},
+  {name:"Sweet Fractions",path:"Fractions-Cafe/index.html",date:"2026-08-27"},
+  {name:"Volcan",path:"Acentos-Volcan/index.html",date:"2026-08-30"},
+  {name:"Unitats",path:"Unitats-Pirata/index.html",date:"2026-08-20"}
+];
